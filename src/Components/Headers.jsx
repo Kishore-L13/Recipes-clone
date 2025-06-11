82d611685a7a953a260dacc692d44e9e7f16def4
kishore <kishore.krrish13@gmail.com>
2025-06-11 23:10:46 +0530

    second commit

---
+++ src/Components/Headers.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+const Headers =() => {
+    return(
+        <div className='flex'>
+            <img />
+            <ul className="flex-auto justify-center">
+                <li className='font-bold'>Products</li>
+                <li>Recipes</li>
+                <li>Inspiration</li>
+                <li>OurIntiative</li>
+                <li>Sign up</li>
+                <li>Search</li>
+            </ul>
+        </div>
+    )
+}
+export default Headers
